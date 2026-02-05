2a9dedccfcbcfb74a21db8395527392a657e783d
Im-breno <brenolessa0509@gmail.com>
2026-02-05 17:39:21 -0300

    Troca de fichas feita

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
 'use strict'
 
-function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-} //*Verifica se é mobile
-
 //* Preview da Imagem
 let previewInputEl = document.querySelector("#fotoInput"); // Botao que adiciona a imagem
 
@@ -17,6 +13,7 @@ function AlternaBotaoApagarImg(){
 }   //alterna o estado de visibilidade do botao de apagar a foto
 
 function previewImportar (e){
+    if (!e.target.files[0]) return;
     urlImgAtual = URL.createObjectURL(e.target.files[0])
     imgPreviewEl.src = urlImgAtual; //importa a imagem
     AlternaBotaoApagarImg(); //deixa o botao de apagar visíveç
@@ -54,9 +51,9 @@ fundoPretoEl.addEventListener('click', ()=>{
     }
     containerNovoExercicioEl.classList.toggle("oculto");
 })
-let listaExEl = document.querySelector('#listaexercicios'); // container da lista
+let lista = document.querySelectorAll('.listaexercicios'); // container da lista
 
-const exerciciosEl = listaExEl.children; //Os exercicios colocados vao estar aqui;
+const exerciciosEl = lista.children; //Os exercicios colocados vao estar aqui;
 
 let editarExercicioEl = document.querySelectorAll('.editarexercicio');
 
@@ -71,117 +68,120 @@ let cargaExEl = document.querySelector('#kg');
 let botaoFecharEl = document.querySelector('#fecharNE');
 
 function criaExercicio(){
-    let nome = NomeExEl.value;
-    let nRep = nRepExEl.value;
-    let carga = cargaExEl.value;
+        let nome = NomeExEl.value;
+        let nRep = nRepExEl.value;
+        let carga = cargaExEl.value;
 
-    let divExEl = document.createElement('div');
-    divExEl.classList.add('exercicio'); // Cria a div do exercicio novo
+        let divExEl = document.createElement('div');
+        divExEl.classList.add('exercicio'); // Cria a div do exercicio novo
 
-    if (estaEditando) {
-        if (editaProximoSibling) listaExEl.insertBefore(divExEl, editaProximoSibling);
-        else listaExEl.appendChild(divExEl);
-        estaEditando = false;
-        editaProximoSibling = null;
-    } else {
-        listaExEl.appendChild(divExEl);
-    }
+        if (estaEditando) {
+            if (editaProximoSibling) lista[fichaAtual].insertBefore(divExEl, editaProximoSibling);
+            else lista[fichaAtual].appendChild(divExEl);
+            estaEditando = false;
+            editaProximoSibling = null;
+        } else {
+            lista[fichaAtual].appendChild(divExEl);
+        }
 
-    let divImgExEl = document.createElement('div');
-    divImgExEl.classList.add('imgEx'); // Cria a div da imagem
-    divExEl.appendChild(divImgExEl);
+        let divImgExEl = document.createElement('div');
+        divImgExEl.classList.add('imgEx'); // Cria a div da imagem
+        divExEl.appendChild(divImgExEl);
 
-    let imgExEl = document.createElement('img');
-    imgExEl.classList.add('imgexercicio');
-    imgExEl.src = urlImgAtual;  //Cria a imagem
-    imgExEl.alt = 'imagem do exercicio';
-    divImgExEl.appendChild(imgExEl);
+        let imgExEl = document.createElement('img');
+        imgExEl.classList.add('imgexercicio');
+        imgExEl.src = urlImgAtual;  //Cria a imagem
+        imgExEl.alt = 'imagem do exercicio';
+        divImgExEl.appendChild(imgExEl);
 
-    let divNomeExEl = document.createElement('div');
-    divNomeExEl.classList.add('nomeEx'); //adiciona a div do nome
-    divExEl.appendChild(divNomeExEl);
+        let divNomeExEl = document.createElement('div');
+        divNomeExEl.classList.add('nomeEx'); //adiciona a div do nome
+        divExEl.appendChild(divNomeExEl);
 
-    let nomeH2El = document.createElement('h2');
-    nomeH2El.textContent = 'Nome';  // titulo nome
-    divNomeExEl.appendChild(nomeH2El)
+        let nomeH2El = document.createElement('h2');
+        nomeH2El.textContent = 'Nome';  // titulo nome
+        divNomeExEl.appendChild(nomeH2El)
 
-    let nomePEl = document.createElement('p');
-    nomePEl.textContent = nome; // Adiciona o nome
-    divNomeExEl.appendChild(nomePEl);
+        let nomePEl = document.createElement('p');
+        nomePEl.textContent = nome; // Adiciona o nome
+        divNomeExEl.appendChild(nomePEl);
 
-    let divKgExEl = document.createElement('div');
-    divKgExEl.classList.add('KgEx'); //adiciona a div do kg
-    divExEl.appendChild(divKgExEl);
+        let divKgExEl = document.createElement('div');
+        divKgExEl.classList.add('KgEx'); //adiciona a div do kg
+        divExEl.appendChild(divKgExEl);
 
-    let kgH2El = document.createElement('h2');
-    kgH2El.textContent = 'Kg';  // titulo kg
-    divKgExEl.appendChild(kgH2El)
+        let kgH2El = document.createElement('h2');
+        kgH2El.textContent = 'Kg';  // titulo kg
+        divKgExEl.appendChild(kgH2El)
 
-    let kgPEl = document.createElement('p');
-    kgPEl.textContent = carga; // Adiciona a carga
-    divKgExEl.appendChild(kgPEl);
+        let kgPEl = document.createElement('p');
+        kgPEl.textContent = carga; // Adiciona a carga
+        divKgExEl.appendChild(kgPEl);
 
-    let divRepExEl = document.createElement('div');
-    divRepExEl.classList.add('RepEx'); //adiciona a div do rep
-    divExEl.appendChild(divRepExEl);
+        let divRepExEl = document.createElement('div');
+        divRepExEl.classList.add('RepEx'); //adiciona a div do rep
+        divExEl.appendChild(divRepExEl);
 
-    let repH2El = document.createElement('h2');
-    repH2El.textContent = 'Rep.';  // titulo rep
-    divRepExEl.appendChild(repH2El)
+        let repH2El = document.createElement('h2');
+        repH2El.textContent = 'Rep.';  // titulo rep
+        divRepExEl.appendChild(repH2El)
 
-    let repPEl = document.createElement('p');
-    repPEl.textContent = nRep; // Adiciona a rep
-    divRepExEl.appendChild(repPEl);
+        let repPEl = document.createElement('p');
+        repPEl.textContent = nRep; // Adiciona a rep
+        divRepExEl.appendChild(repPEl);
 
-    let divCheckBoxExEl = document.createElement('div');
-    divCheckBoxExEl.classList.add('checkboxEx'); // Adiciona a div da checkbox
-    divExEl.appendChild(divCheckBoxExEl);
+        let divCheckBoxExEl = document.createElement('div');
+        divCheckBoxExEl.classList.add('checkboxEx'); // Adiciona a div da checkbox
+        divExEl.appendChild(divCheckBoxExEl);
 
-    let checkBoxExEl = document.createElement('input');
-    checkBoxExEl.type = "checkbox";
-    checkBoxExEl.name = "checkboxEx";   // Adiciona a checkbox
-    checkBoxExEl.classList.add('checkboxEx');
-    divCheckBoxExEl.appendChild(checkBoxExEl);
+        let checkBoxExEl = document.createElement('input');
+        checkBoxExEl.type = "checkbox";
+        checkBoxExEl.name = "checkboxEx";   // Adiciona a checkbox
+        checkBoxExEl.classList.add('checkboxEx');
+        divCheckBoxExEl.appendChild(checkBoxExEl);
 
-    let divEditarExEl = document.createElement('div');
-    divEditarExEl.classList.add('editarexercicio');
-    divExEl.appendChild(divEditarExEl);  //adiciona a div do editar
+        let divEditarExEl = document.createElement('div');
+        divEditarExEl.classList.add('editarexercicio');
+        divExEl.appendChild(divEditarExEl);  //adiciona a div do editar
 
-    let botaoExcluir = document.createElement('button');
-    botaoExcluir.type = 'button'; //cria o botao de excluir
-    botaoExcluir.classList.add('excluirEx');
-    divEditarExEl.appendChild(botaoExcluir);
+        let botaoExcluir = document.createElement('button');
+        botaoExcluir.type = 'button'; //cria o botao de excluir
+        botaoExcluir.classList.add('excluirEx');
+        divEditarExEl.appendChild(botaoExcluir);
 
-    let imgExcluir = document.createElement('img');
-    imgExcluir.alt = 'Excluir Exercicio'; // Cria a imagem do botao de excluir
-    imgExcluir.src = 'img/apagarfotoicone.png';
-    botaoExcluir.appendChild(imgExcluir);
+        let imgExcluir = document.createElement('img');
+        imgExcluir.alt = 'Excluir Exercicio'; // Cria a imagem do botao de excluir
+        imgExcluir.src = 'img/apagarfotoicone.png';
+        botaoExcluir.appendChild(imgExcluir);
 
-    let botaoEditar = document.createElement('button');
-    botaoEditar.type = 'button';    //Cria o botao de editar
-    botaoEditar.classList.add('editarEx');
-    divEditarExEl.appendChild(botaoEditar);
+        let botaoEditar = document.createElement('button');
+        botaoEditar.type = 'button';    //Cria o botao de editar
+        botaoEditar.classList.add('editarEx');
+        divEditarExEl.appendChild(botaoEditar);
 
-    let imgEditar = document.createElement('img');
-    imgEditar.alt = 'Editar foto';  // Cria a imagem de do botao editar
-    imgEditar.src = 'img/editarExicone.png';
-    botaoEditar.appendChild(imgEditar);
+        let imgEditar = document.createElement('img');
+        imgEditar.alt = 'Editar foto';  // Cria a imagem de do botao editar
+        imgEditar.src = 'img/editarExicone.png';
+        botaoEditar.appendChild(imgEditar);
 
-    NomeExEl.value = '';
-    nRepExEl.value = ''; // reseta as informações do editar imagem
-    cargaExEl.value = '';
+        NomeExEl.value = '';
+        nRepExEl.value = ''; // reseta as informações do editar imagem
+        cargaExEl.value = '';
 
-    previewImportarRemover();
-    containerNovoExercicioEl.classList.toggle("oculto"); // Esconde a aba de gerenciar
+        previewImportarRemover();
+        containerNovoExercicioEl.classList.toggle("oculto"); // Esconde a aba de gerenciar
 
-    editarExercicioEl = document.querySelectorAll('.editarexercicio');
+        botaoApagarImgEl.classList.add('oculto');
+
+        editarExercicioEl = document.querySelectorAll('.editarexercicio');
 }
 
 //* Cria um novo exercicio
 botaoFecharEl.addEventListener('click', criaExercicio)
 //* Cria um novo exercicio
 
-    listaExEl.addEventListener('click', (e) => {
+lista.forEach((listaEl) => {
+    listaEl.addEventListener('click', (e) => {
         const btnExcluir = e.target.closest('.excluirEx');
         if (!btnExcluir) return;
     
@@ -189,7 +189,7 @@ botaoFecharEl.addEventListener('click', criaExercicio)
         if (card) card.remove();
     });
 
-    listaExEl.addEventListener('click', (e) => {
+    listaEl.addEventListener('click', (e) => {
         const btnEditar = e.target.closest('.editarEx');
         if (!btnEditar) return;
 
@@ -219,3 +219,15 @@ botaoFecharEl.addEventListener('click', criaExercicio)
         containerNovoExercicioEl.classList.remove("oculto");
         exercicio.remove();
     });
+});
+
+let trocarFichaBotaoEl = document.querySelectorAll('.trocaficha');
+
+let fichaAtual = 0;
+
+for (let i = 0; i < trocarFichaBotaoEl.length; i++) {
+    trocarFichaBotaoEl[i].addEventListener('click', () => {
+        lista[fichaAtual].classList.add('oculto');
+        fichaAtual = i;
+        lista[fichaAtual].classList.remove('oculto');
+    })}
